7d3bc5718a29a53afb5a1f276a2c1aaec04ca148
Simon Bullado <102648783+simbld@users.noreply.github.com>
2023-12-09 03:27:16 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav>
-      <ul>{/* Add your links here */}</ul>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user-profile/myprofil">myprofil</Link>
+        </li>
+        <li>
+          <Link to="/user-profile/gaeron">gaeron</Link>
+        </li>
+        <li>
+          <Link to="/user-profile/tonton">tonton</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
